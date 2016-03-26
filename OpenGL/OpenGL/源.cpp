8d13709cf1c5ad13a71f2c8d119dab87c948ea71
xue blood <xue.blood@outlook.com>
2016-03-26 17:46:21 +0800

    canvas class

--- OpenGL/OpenGL/源.cpp
+++ OpenGL/OpenGL/源.cpp
@@ -6,52 +6,66 @@
 
 #define _ScreeenWidth 640
 #define _ScreenHeight 480
+
 #define _ScreenCenter _ScreeenWidth/2,_ScreenHeight/2
 
 #define _Length 30
 
+#define turn(r) cvs.Turn(r)
+
 Canvas cvs(_ScreeenWidth, _ScreenHeight, "Canvas");
 
-void draw(char *str)
+void drawA(GLbyte _loop)
 {
-	for (GLsizei i = 0; str[i];i++)
+#define _LengthA 30
+#define _LengthB 20
+#define _LengthC 10
+
+#define aDrawA cvs.Forward(_LengthA,1)
+#define aDrawB cvs.Forward(_LengthB,1)
+#define aDrawC cvs.Forward(_LengthC,1)
+
+	cvs.MoveTo(0,10);
+
+	for (GLbyte i = 0; i < _loop;i++)
 	{
-		switch (str[i])
-		{
-		case 'F':
-			cvs.Forward(_Length, 1);
-			break;
-		case 'L':
-			cvs.Turn(60);
-			break;
-		case 'R':
-			cvs.Turn(-60);
-			break;
-		default:
-			break;
-		}
+		cvs.TurnTo(0);
+
+		aDrawA; turn(90);
+		aDrawB; turn(90);
+		aDrawC; turn(90);
+		aDrawC; turn(-90);
+
+		aDrawC; turn(-90);
+		aDrawB; turn(-90);
+		aDrawA; turn(-90);
+		aDrawA;
+	}
+}
+void polyspiral(GLfloat _len,GLfloat _angle,GLfloat _inc,GLint _num)
+{
+	cvs.MoveTo(_ScreenCenter);
+	cvs.TurnTo(0);
+
+	for (GLint i = 0; i < _num; i++)
+	{
+		cvs.Forward(_len + i*_inc, 1);
+		cvs.Turn(_angle);
 	}
 }
 
-#define _TempFile "r:\\temp.txt"
-#define _Buff_Size 100
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT); // clear the screen
 
-	FILE *file;
-	fopen_s(&file, _TempFile, "r");
+	glColor3f(1.0, .0, .0);
+	drawA(30);
 
-	char buffer[_Buff_Size] = { 0 };
-	
-	cvs.MoveTo(_ScreenCenter);
-	
-	while (fread_s(buffer, _Buff_Size, sizeof(char), 1, file))
-		draw(buffer);
+	glColor3f(.0, 1.0, .0);
+	polyspiral(3, 30, 3, 30);
 
 	glutSwapBuffers();	// swap the color buffer
-	fclose(file);
 
 }
 void main(int argc, char **argv)
